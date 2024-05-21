3b89de7465a8f89484e01fcc1a8acaaadd716fa6
Nikita Kuchur <nikitakuchur@gmail.com>
2024-05-21 14:00:49 +0200

    validation: disallow usernames with whitespaces

--- user-service/src/main/java/com/github/nikitakuchur/userservice/model/dto/SignupRequest.java
+++ user-service/src/main/java/com/github/nikitakuchur/userservice/model/dto/SignupRequest.java
@@ -2,6 +2,7 @@ package com.github.nikitakuchur.userservice.model.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -13,6 +14,7 @@ public class SignupRequest {
     @Email
     private String email;
     @NotBlank
+    @Pattern(regexp = "^\\S*$", message = "The username cannot contain any whitespaces")
     private String username;
     @NotBlank
     @Length(min = 8)
