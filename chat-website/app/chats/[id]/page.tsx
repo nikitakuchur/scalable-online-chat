04fb7b46860db513831f8c15d7060162f08e4a15
Nikita Kuchur <nikitakuchur@gmail.com>
2024-05-24 20:40:37 +0200

    sending and receiving messages

--- chat-website/app/chats/[id]/page.tsx
+++ chat-website/app/chats/[id]/page.tsx
@@ -1,53 +1,146 @@
+'use client'
+
 import {Textarea} from "@nextui-org/input";
 import {Button} from "@nextui-org/button";
 import {Card, CardBody, CardHeader} from "@nextui-org/card";
-import {Metadata} from "next";
+import {FormEvent, useContext, useEffect, useRef, useState} from "react";
+import {Client} from "@stomp/stompjs";
+import {AuthContext} from "@/app/providers";
+import {useParams} from "next/navigation";
+import {jwtFetch, retrieveTokens} from "@/utils/jwt-fetch";
+import {Spinner} from "@nextui-org/spinner";
 
-export const metadata: Metadata = {
-	title: 'Chat',
+interface Message {
+	id?: string,
+	sender?: string,
+	text: string,
 }
 
-export default function ChatPage({ params }: { params: { id: string } }) {
-	const username = "User";
-	const messages = [
-		{
-			id: 1,
-			sender: "John",
-			text: "Hi guys! How are you doing today?"
-		},
-		{
-			id: 2,
-			sender: "User",
-			text: "It's working!"
-		},
-		{
-			id: 3,
-			sender: "Damon",
-			text: "Woo-hoo!"
-		},
-	]
+export default function ChatPage() {
+	const params = useParams<{ id: string; }>()
+	const { user} = useContext(AuthContext);
+
+	const notificationRef = useRef<HTMLAudioElement>();
+	const [ chatName, setChatName ] = useState<string>("");
+
+	const [ loading, setLoading ] = useState<boolean>(true);
+	const [ text, setText ] = useState<string>("");
+
+	const clientRef = useRef<Client>();
+	const [messages, setMessages] = useState<Message[]>([]);
+
+	async function retrieveChatInfo() {
+		const response = await jwtFetch(`/api/chats/${params.id}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		});
+		if (response.ok) {
+			const chat: Chat = await response.json();
+			setChatName(chat.name);
+		} else {
+			console.error("An error occurred while retrieving the chat info.")
+		}
+	}
+
+	function playNotification() {
+		const notification = notificationRef.current;
+		if (!notification) {
+			return;
+		}
+		if (notification.paused) {
+			notification.play();
+		} else {
+			notification.currentTime = 0
+		}
+	}
+
+	function establishConnection() {
+		if (clientRef.current != null) {
+			return () => {};
+		}
+
+		const client = new Client({
+			brokerURL: 'ws://localhost:8083/chat',
+			beforeConnect: () => {
+				const accessToken = localStorage.getItem("accessToken");
+				client.connectHeaders = {
+					Authorization: `Bearer ${accessToken}`,
+				}
+			},
+			onConnect: () => {
+				console.log("Connected!");
+				setLoading((value) => false);
+				client.subscribe(`/topic/${params.id}`, message => {
+					const newMessage: Message = JSON.parse(message.body);
+					setMessages((messages) => [newMessage, ...messages]);
+					playNotification();
+				});
+			},
+			onStompError: frame => {
+				console.error("An error occurred while connecting to the chat.");
+				retrieveTokens();
+			}
+		});
+
+		client.activate();
+		clientRef.current = client;
+	}
+
+	useEffect(() => {
+		notificationRef.current = new Audio("/notification.mp3");
+
+		retrieveChatInfo()
+			.then(() => establishConnection());
+
+		return () => {
+			if (clientRef.current != null) {
+				clientRef.current.forceDisconnect();
+			}
+		}
+	}, []);
+
+	function handleSubmit(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		if (user == null || text.trim() == "") {
+			return;
+		}
+
+		const message: Message = {
+			text: text,
+		};
+		clientRef.current?.publish({ destination: `/app/${params.id}`, body: JSON.stringify(message) });
+		setText("");
+	}
 
 	return (
 		<div className="flex flex-col w-full h-full">
-			<h2 className="text-center text-2xl font-bold leading-9 tracking-tight">Chat {params.id}</h2>
+			<h2 className="text-center text-2xl font-bold leading-9 tracking-tight">{chatName}</h2>
 			<div className="p-4 flex flex-col-reverse gap-y-2 flex-nowrap flex-grow overflow-scroll h-96">
-				{messages.reverse().map(message =>
-					<Card key={message.id} className={"max-w-96 shrink-0 " + (message.sender !== username ? "self-start" : "self-end")} shadow="sm">
-						{ message.sender !== username && (<CardHeader className="text-xs pb-0">{message.sender}</CardHeader>)}
+				{messages.map(message =>
+					<Card key={message.id} className={"max-w-96 shrink-0 " + (message.sender !== user?.sub ? "self-start" : "self-end")} shadow="sm">
+						{ message.sender !== user?.sub && (<CardHeader className="text-xs pb-0">{message.sender}</CardHeader>)}
 						<CardBody>
 							<p>{message.text}</p>
 						</CardBody>
 					</Card>
 				)}
 			</div>
-			<div className="flex flex-row gap-x-2 items-end mb-4">
+			{ loading && <Spinner className="z-50 absolute inset-0" size="lg" /> }
+			<form className="flex flex-row gap-x-2 items-end mb-4" onSubmit={handleSubmit}>
 				<Textarea
+					readOnly={loading}
 					placeholder="Write a message"
 					className="flex-auto"
 					minRows={1}
+					name="text"
+					value={text}
+					onChange={event => setText(event.target.value)}
 				/>
-				<Button>Send</Button>
-			</div>
+				<Button type="submit">Send</Button>
+			</form>
 		</div>
 	);
 }
--- chat-website/utils/jwt-fetch.ts
+++ chat-website/utils/jwt-fetch.ts
@@ -1,29 +1,44 @@
 export const jwtFetch: typeof fetch = async (url, params) => {
     const accessToken = localStorage.getItem("accessToken");
-
     (params?.headers as any)["Authorization"] = `Bearer ${accessToken}`;
     let data = await fetch(url, params);
 
-    if (data.status == 403) {
-        const tokenResponse = await fetch('/api/refresh-token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ refreshToken: localStorage.getItem("refreshToken") })
-        });
-        if (!tokenResponse.ok) {
-            const error = "An error occurred while retrieving new tokens.";
-            console.error(error, await tokenResponse.text());
-            return Promise.reject(error);
-        }
-        const tokens = await tokenResponse.json();
-        localStorage.setItem("accessToken", tokens.accessToken);
-        localStorage.setItem("refreshToken", tokens.refreshToken);
-
-        (params?.headers as any)["Authorization"] = `Bearer ${tokens.accessToken}`;
+    let count = 0;
+    while (data.status == 403 && count < 5) {
+        await retrieveTokens();
+        const accessToken = localStorage.getItem("accessToken");
+        (params?.headers as any)["Authorization"] = `Bearer ${accessToken}`;
         data = await fetch(url, params);
+        if (data.status != 403) {
+            break;
+        }
+        await sleep(1000);
+        count++;
     }
 
     return data;
+}
+
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+/**
+ * Retrieves access and refresh token and puts them into the local storage.
+ */
+export async function retrieveTokens() {
+    console.log("Retrieving tokens...")
+    const tokenResponse = await fetch('/api/refresh-token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ refreshToken: localStorage.getItem("refreshToken") })
+    });
+    if (!tokenResponse.ok) {
+        const error = "An error occurred while retrieving new tokens.";
+        console.error(error, await tokenResponse.text());
+        return Promise.reject(error);
+    }
+    const tokens = await tokenResponse.json();
+    localStorage.setItem("accessToken", tokens.accessToken);
+    localStorage.setItem("refreshToken", tokens.refreshToken);
 }
